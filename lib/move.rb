504dced75e22d0d53c6537233f20b63401afc205
Tommy Jake <tomlom1984@hotmail.co.uk>
2019-07-29 13:40:03 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,8 +8,6 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index (user_input)
-  user_input = gets.chomp
-  user_input.downcase!
   converted_input = user_input.to_i
   converted_input - 1
 end
