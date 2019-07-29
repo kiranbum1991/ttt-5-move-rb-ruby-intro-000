91f84fb3bc990806dc9058825633ca33c535dcbb
Tommy Jake <tomlom1984@hotmail.co.uk>
2019-07-29 13:23:39 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,7 +8,8 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index (user_input)
-  user_input.gets.chomp.to_i - 1
+  user_input.gets.chomp.to_i
+  user_input - 1
 end
 
 def move
